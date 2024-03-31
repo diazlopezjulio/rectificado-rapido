782831514941e4e55e0dd1325547410ed2b76a63
diazlopezjulio <juliocesardiaz44@gmail.com>
2024-03-31 00:35:31 -0700

    Se añade validacion de fs y path en el servidor.

--- src/app/core/services/data-sync.service.ts
+++ src/app/core/services/data-sync.service.ts
@@ -2,8 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { PLATFORM_ID, Injectable, Inject } from '@angular/core';
 import { RootObject } from '../../shared/models/vehiculo-tipo.model';
 import { isPlatformServer } from '@angular/common';
-import * as fs from 'fs';
-import * as path from 'path';
+
 
 @Injectable({
 	providedIn: 'root'
@@ -11,7 +10,7 @@ import * as path from 'path';
 export class DataSyncService {
 	private readonly JSON_URL = '/assets/file.json';
 
-	constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: string) { }
+	constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: any) { }
 
 	ngOnInit() {
 		this.http.get(this.JSON_URL).subscribe(data => {
@@ -30,6 +29,10 @@ export class DataSyncService {
 			const jsonData = JSON.stringify(data, null, 2); // Convierte el objeto a una cadena JSON
 
 			if (isPlatformServer(this.platformId)) {
+				// eslint-disable-next-line
+				const fs = require('fs');
+				// eslint-disable-next-line
+				const path = require('path');
 				const filePath = path.join(process.cwd(), 'src', 'assets', 'file.json');
 				fs.writeFile(filePath, jsonData, 'utf8', (err: Error | null) => {
 					if (err) {
